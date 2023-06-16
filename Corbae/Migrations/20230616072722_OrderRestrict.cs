ec8844497030de311b8dc8fba412a7947385c3ab
SickleShaped <sickleshaped@gmail.com>
2023-06-16 10:41:17 +0300

    fix

---
+++ Corbae/Migrations/20230616072722_OrderRestrict.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Corbae.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderRestrict : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Orders_OrderID",
+                table: "OrderProducts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Products_ProductID",
+                table: "OrderProducts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserID",
+                table: "Orders");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Orders_OrderID",
+                table: "OrderProducts",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "OrderID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Products_ProductID",
+                table: "OrderProducts",
+                column: "ProductID",
+                principalTable: "Products",
+                principalColumn: "ProductID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserID",
+                table: "Orders",
+                column: "UserID",
+                principalTable: "Users",
+                principalColumn: "UserID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Orders_OrderID",
+                table: "OrderProducts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Products_ProductID",
+                table: "OrderProducts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserID",
+                table: "Orders");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Orders_OrderID",
+                table: "OrderProducts",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "OrderID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Products_ProductID",
+                table: "OrderProducts",
+                column: "ProductID",
+                principalTable: "Products",
+                principalColumn: "ProductID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserID",
+                table: "Orders",
+                column: "UserID",
+                principalTable: "Users",
+                principalColumn: "UserID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
